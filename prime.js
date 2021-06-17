01675959d6dde3de0055a4ed6dab53bc24d12fb3
imac <mrrobot@192.168.10.100>
2021-06-17 10:10:34 +0900

    nodejs

--- prime.js
+++ prime.js
@@ -1,20 +1,38 @@
 "use strict";
 
-let n = parseInt(process.argv[2]);
+const keypress = require('keypress');
+var n = parseInt(process.argv[2]);
+if (!process.argv[2]) {
+    console.log("自然数を入力してください");
+    keypress(process.stdin);
+    process.stdin.resume();
+    process.stdin.setEncoding('utf8');
+    process.stdin.on('data', function(chunk) {
+        n = chunk;
+    });
+    process.stdin.on('keypress', function(key) {
+        prime(n);
+        process.exit(1);
+    });
+}
 if (n <= 2) {
     console.log("引数は2より大きい数値にしてください:");
     process.exit(0);
 }
 
-for (let i = 2; i < n;) {
-    if ((n % i) === 0) {
-        n = n / i;
-        console.log(i);
-    } else {
-        i++;
-    }
-    if (n === i) {
-        console.log(i);
-        break;
+prime(n);
+
+function prime(n) {
+    for (let i = 2; i < n;) {
+        if ((n % i) === 0) {
+            n = n / i;
+            console.log(i);
+        } else {
+            i++;
+        }
+        if (n === i) {
+            console.log(i);
+            break;
+        }
     }
 }
